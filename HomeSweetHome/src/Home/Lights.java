adb85e49fb76fdb490b821bc773144077bb48327
Jakub Własny <34214@KTIAIRPJ-015.ue.poznan>
2016-04-20 16:18:22 +0200

    Lights and Project classes added

---
+++ HomeSweetHome/src/Home/Lights.java
@@ -0,0 +1,17 @@
+package Home;
+
+public class Lights implements Device {
+
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ HomeSweetHome/src/Home/Projector.java
@@ -0,0 +1,25 @@
+package Home;
+
+public class Projector implements Device {
+
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void wideScreenMode() {
+		
+	}
+	
+	public void tvMode() {
+		
+	}
+
+}
