78d411252fa9c8e09cb057d7e5c5d6a01cbb7181
34854 <34854@KTIPK-078.ue.poznan>
2016-04-20 16:21:50 +0200

    zmiana 2

---
+++ HomeSweetHome/src/Home/CdPlayer.java
@@ -0,0 +1,41 @@
+package Home;
+
+public class CdPlayer implements Device {
+	
+	
+	public void eject(){
+		
+		
+		
+	}
+
+	public void play(String cd){
+		
+		
+		
+	}
+	
+	public void stop(){
+		
+		
+		
+	}
+	
+	public void pause(){
+		
+		
+		
+	}
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ HomeSweetHome/src/Home/Tuner.java
@@ -0,0 +1,35 @@
+package Home;
+
+public class Tuner implements Device{
+	
+	public void setAm(){
+		
+		
+		
+	}
+
+	public void setFm(){
+		
+		
+		
+	}
+	
+	public void setFrequency(){
+		
+		
+		
+	}
+	
+	@Override
+	public void on() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void off() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
